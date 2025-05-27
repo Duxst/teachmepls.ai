2e4acb0131a726a40b1aaafddba1af6a7d3e5080
Abdalla_Abdalla <abdalla7294@outlook.com>
2025-05-27 00:05:56 -0500

    Update trademark from TeachMePls.ai to Savant

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -28,7 +28,7 @@ const Layout = ({ children }: LayoutProps) => {
               </a>
             </div>
             <p className="mt-8 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">
-              &copy; 2024 TeachMePls.ai. All rights reserved.
+              &copy; 2024 Savant. All rights reserved.
             </p>
           </div>
         </div>
