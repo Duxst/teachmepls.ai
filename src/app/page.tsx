af60ce7da816b58d412162a5fe8657f160ec423a
Abdalla_Abdalla <abdalla7294@outlook.com>
2025-05-27 01:36:07 -0500

    Make biocard image larger and remove lookatcard from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,16 +43,8 @@ export default function Home() {
               </Button>
             </div>
           </div>
-          <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
-            <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-              <Image
-                src="/hero-image.svg"
-                alt="App screenshot"
-                width={2432}
-                height={1442}
-                className="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
-              />
-            </div>
+          <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32 justify-center items-center">
+            <Image src="/biocard.png" alt="Bio Card" width={520} height={390} className="rounded-lg object-cover" />
           </div>
         </div>
       </div>
